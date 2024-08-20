5c8e355bed4ead86d4c4b50cc0f38c99b72b7adb
BeshoyMarkElMallah <besho.elmallah@gmail.com>
2024-08-20 12:42:10 +0300

    add Auth context

--- src/graphql/user/resolvers.ts
+++ src/graphql/user/resolvers.ts
@@ -2,15 +2,27 @@ import UserService, { CreateUserPayload } from "../../services/user";
 const queries = {
     getUserToken: async (_: any, payload: { email: string, password: string }) => {
         const token = await UserService.getUserToken({
-            email:payload.email,
-            password:payload.password
+            email: payload.email,
+            password: payload.password
         });
         return token;
+    },
+    getCurrentLoggedInUser: async (_: any, parameters: any, context: any) => {
+        console.log(context);
+        if(context && context.user){
+            const id = context.user.id;
+            const user  = await UserService.getUserById(id);
+
+            return user;
+        }
+        throw new Error("I don't know who are you")
     }
+
+
 }
 
 const mutations = {
-    createUser: async(_:any,payload: CreateUserPayload) => {
+    createUser: async (_: any, payload: CreateUserPayload) => {
         const res = await UserService.createUser(payload);
         return res.id
     }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import createApolloGraphqlServer from './graphql'
+import UserService from './services/user';
 
 async function init() {
 
@@ -21,7 +22,20 @@ async function init() {
         })
     })
 
-    app.use('/graphql', expressMiddleware(gqlServer));
+    app.use('/graphql', expressMiddleware(gqlServer, {
+        context: async ({req}) => {
+            // @ts-ignore
+            const token = req.headers['token'];
+            // console.log(req.headers);
+            
+            try {
+                const user = UserService.decodeJWTToken(token as string);
+                return {user};
+            } catch (error) {
+                return {}
+            }
+        }
+    }));
 
 
     app.listen(PORT, () => console.log(`Server started at PORT:${PORT}`)
